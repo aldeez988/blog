367c43c713d8b5c0e3f22e2d448ad77ace3189af
aldeez988 <aldeez911088@gmail.com>
2019-11-03 21:59:39 +0000

    rendering posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,8 +5,23 @@ class PostList extends Component {
   componentWillMount(){
     this.props.fetchPosts();
   }
+  renderList(){
+return this.props.posts.map(post=>(
+  <div className="item" key={post.id}>
+    <i className="large middle aligned icon user"/>
+    <div className="content">
+      <div className="description">
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+    </div>
+  </div>
+))
+  }
   render() { 
-    return (<div>Post List</div>);
+    return (<div className="ui relaxed divided list">
+      {this.renderList()}
+    </div>);
   }
 }
  const mapStateToProps = ({posts})=>{
