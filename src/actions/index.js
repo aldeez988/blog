8aa8fbcf184dddfe290f0fc2ddd13db4a0fb8c4e
aldeez988 <aldeez911088@gmail.com>
2019-11-09 17:17:04 +0000

    refactoring fetchUser action with and addin memoize function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,18 @@
-import _ from 'lodash';
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/posts');
+  const response = await jsonPlaceholder.get("/posts");
   dispatch({
-    type: 'FETCH_POSTS',
+    type: "FETCH_POSTS",
     payload: response.data
   });
 };
-export const fetchUser = (id)=> async dispatch =>{
-const response = await jsonPlaceholder.get(`/users/${id}`);
-dispatch({
-  type: 'FETCH_USER',
-  payload: response.data
+export const fetchUser = id => dispatch =>_fetchUSer(id, dispatch); 
+
+const _fetchUSer = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data
+  });
 });
-}
